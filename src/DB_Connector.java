4ad499191674b7884a4582e25e4467252225be9e
Thom9521 <Thom9521@edu.easj.dk>
2018-02-20 14:25:53 +0100

    Initial commit

---
+++ src/DB_Connector.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+
+
+public class DB_Connector {
+
+    private static String url = "jdbc:postgresql://localhost:5432/mydatabase";
+    private static String user = "postgres";
+    private static String pass = "xmeto1234";
+    private static Connection conn = null;
+
+    public static Connection connect() {
+        System.out.println("\n--Connecting to PostgresSQL JDBC--");
+
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+        if (conn != null) {
+            System.out.println("\n--Connection successful--");
+        } else {
+            System.out.println("\n--Connection failed--");
+        }
+        return conn;
+    }
+}
+
