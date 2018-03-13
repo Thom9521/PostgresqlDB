724c3a4cb00483ee5c177a6b1622fce7542f2941
Thom9521 <Thom9521@edu.easj.dk>
2018-03-13 12:41:03 +0100

    Ekstra

--- src/Connection_Test.java
+++ src/Connection_Test.java
@@ -2,6 +2,18 @@ import java.sql.Connection;
 
 public class Connection_Test {
     public static void main(String[] args) {
-        Connection conn = DB_Connector.connect();
+
+        //  Test DB_Connector
+//        Connection conn = DB_Connector.connect();
+
+        //  Create a DB_Connector object
+//        DB_Connector connect = new DB_Connector();
+
+        //  Test DB_Statements - insertData and retrieveData methods
+        DB_Statements statements = new DB_Statements();
+        statements.insertData();
+        statements.retrieveData();
+
+
     }
 }
--- src/DB_Connector.java
+++ src/DB_Connector.java
@@ -8,23 +8,56 @@ public class DB_Connector {
     private static String pass = "";
     private static Connection conn = null;
 
+    //    Method that creates & opens a connection to postgresql db
     public static Connection connect() {
-        System.out.println("\n--Connecting to PostgresSQL JDBC--");
+
+        System.out.println("\n--Connecting to PostgreSQL JDBC--");
 
         try {
-            conn = DriverManager.getConnection(url, user, pass);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            //  Check connection credentials
+            conn = DriverManager.getConnection(url, user, pass);
+            System.out.println("\n--JDBC driver registered--");
 
         }
+        //  Handle possible exceptions
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error : " + e);
 
+        }
+        //  Check if there is a connection
         if (conn != null) {
             System.out.println("\n--Connection successful--");
+            System.out.println("\n--You are logged in as " + user + "--");
+            System.out.println("\n--Logged into: MyDatabase--");
         } else {
             System.out.println("\n--Connection failed--");
         }
+
         return conn;
     }
-}
 
+    //  Method that closes the connection if exists
+    public void closeConnection() {
+
+        try {
+
+        }
+        finally {
+            try {
+                System.out.println("\n--Closing connection to database--");
+                if (conn != null) {
+                    conn.close();
+                    System.out.println("\n--Connection closed--");
+
+                }
+            }
+            catch(SQLException e) {
+                e.printStackTrace();
+                System.out.println("\n--Error - closing connection failed--");
+            }
+        }
+    }
+
+}
